13e6de466d466c7886f02e0cb039f6b1b769efdc
yashchalke <chalkeyashkrishna@gmail.com>
2025-07-13 21:11:51 +0530

    Created dummy product card

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <div className='w-70 h-80 border px-3 py-3 flex-col justify-center'>
+        <div className='w-full h-60 border rounded-2xl '>
+
+        </div>
+        <div className='border mt-3 h-12'>
+            <h1>Title</h1>
+            <h1>Price</h1>
+        </div>
+    </div>
   )
 }
 
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import ProductCard from '../components/ProductCard'
 
 const Cart = () => {
+
+  const cartitems = [
+    "test-1","test-2","test-3","test-4","test-5"
+  ]
+
   return (
-    <div>Cart</div>
+    <>
+    <div className='w-full flex flex-col items-center border px-10 py-10 gap-y-4 lg:flex lg:flex-row lg:gap-x-3'>
+      {
+        cartitems.map((items)=> (
+          <ProductCard />
+        )
+        )
+      }
+    </div>
+    </>
   )
 }
 
