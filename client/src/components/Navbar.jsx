dc4e05be1dcd28129d5283d0c26607a450829ef6
yashchalke <chalkeyashkrishna@gmail.com>
2025-07-30 23:44:03 +0530

    Changes in Navbar for Search

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,18 +11,18 @@ const Navbar = () => {
   return (
     <div className='sticky top-0 z-50 w-full h-[50px] md:h-[80px] bg-white border'>
         <div className='flex justify-between items-center mt-2 mx-4 h-8 md:h-12 md:mt-4 md:mx-[40px] '>
-        <div className='flex gap-2 lg:gap-8 items-center'>
+        <div className='flex items-center gap-2 lg:gap-8'>
             <div className='md:hidden'>
                 <button onClick={()=> {SetIsOpen(!isOpen)}}><Menu /></button>
                 
                 {isOpen && (
                     <>
                     <div
-            className="fixed inset-0 bg-white opacity-60 z-40"
+            className="fixed inset-0 z-40 bg-white opacity-60"
             onClick={handleClose}
           />
-          <div className='fixed top-0 left-0 w-64 h-full bg-white z-50 p-6 flex flex-col gap-4 shadow-lg animate-slideIn'>
-            <button onClick={handleClose} className="mb-4 self-end" aria-label="Close menu">
+          <div className='fixed top-0 left-0 z-50 flex flex-col w-64 h-full gap-4 p-6 bg-white shadow-lg animate-slideIn'>
+            <button onClick={handleClose} className="self-end mb-4" aria-label="Close menu">
               <X size={24} />
             </button>
             <Link to="/Shop" className="py-2 border-b" onClick={handleClose}>Shop</Link>
@@ -40,17 +40,17 @@ const Navbar = () => {
                 )
                 }
             </div>
-        <div className='font-extrabold text-2xl md:text-4xl md:ml-2 '><Link to="/">Shop</Link></div>
-        <div className="hidden lg:flex gap-5 ">
-            <Link to="/Shop" className='text-gray-500 hover:text-black hover:underline  transition-colors duration-200'>Shop</Link>
-            <Link to="/OnSale" className='text-gray-500 hover:text-black hover:underline transition-colors duration-200'>On sale</Link>
-            <Link to="/New-Arrivals" className='text-gray-500 hover:text-black hover:underline transition-colors duration-200'>New Arrivals</Link>
-            <Link to="/Brands" className='text-gray-500 hover:text-black hover:underline transition-colors duration-200'>Brands</Link>
+        <div className='text-2xl font-extrabold md:text-4xl md:ml-2 '><Link to="/">Shop</Link></div>
+        <div className="hidden gap-5 lg:flex ">
+            <Link to="/Shop" className='text-gray-500 transition-colors duration-200 hover:text-black hover:underline'>Shop</Link>
+            <Link to="/OnSale" className='text-gray-500 transition-colors duration-200 hover:text-black hover:underline'>On sale</Link>
+            <Link to="/New-Arrivals" className='text-gray-500 transition-colors duration-200 hover:text-black hover:underline'>New Arrivals</Link>
+            <Link to="/Brands" className='text-gray-500 transition-colors duration-200 hover:text-black hover:underline'>Brands</Link>
         </div>
         </div>
         
         <div className='flex items-center gap-6'>
-            <div className='md:hidden'><Search /></div>
+            <div className='md:hidden'><Search /></div> {/*After clicking on search button , search bar should open to get the user input */}
             <div className='hidden md:flex'>
             <input type="text" placeholder='Search' className='w-80 h-10 rounded-[20px] border bg-gray-300 text-black pl-3'></input>
         </div>
